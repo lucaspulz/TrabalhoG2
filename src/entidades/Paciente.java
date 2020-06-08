7fa34957a8f05f624959adabf406dcfa3d1b8908
lucaspulz <lucaspulz@hotmail.com>
2020-06-08 15:50:24 -0300

    Criação da Classe Remedio

--- src/entidades/Paciente.java
+++ src/entidades/Paciente.java
@@ -6,6 +6,9 @@ public class Paciente extends Pessoa {
 	
 	private Doenca doenca;
 	private Quarto quarto;
+	private Remedio remedio;
+	
+	
 	
 	
 	public Paciente() {
@@ -15,11 +18,25 @@ public class Paciente extends Pessoa {
 	
 	
 	public Paciente(int id,String nome, Contato contato, Endereco endereco, Date dataNascimento, int rg, int cpf,
-			Doenca doenca, Quarto quarto) {
+			Doenca doenca, Quarto quarto,Remedio remedio) {
 		super(id,nome, contato, endereco, dataNascimento, rg, cpf);
 		this.doenca = doenca;
 		this.quarto = quarto;
+		this.remedio =  remedio;
 	}
+	
+	
+	public Remedio getRemedio() {
+		return remedio;
+	}
+
+
+
+	public void setRemedio(Remedio remedio) {
+		this.remedio = remedio;
+	}
+
+
 
 	public Doenca getDoenca() {
 		return doenca;
---
+++ src/entidades/Remedio.java
@@ -0,0 +1,16 @@
+package entidades;
+
+public class Remedio {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
